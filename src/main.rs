ad79a51654fb7136a227ef143277dbbd929ea5e7
isso0424 <isso04240129@gmail.com>
2023-04-10 05:51:39 +0900

    feat: imple FirstController

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use prac::first_controller_server::{FirstController, FirstControllerServer};
+use prac::{HelloRequest, HelloResponse};
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod prac {
+    tonic::include_proto!("prac");
+}
+
+#[derive(Debug, Default)]
+pub struct ImplFirstController {}
+
+#[tonic::async_trait]
+impl FirstController for ImplFirstController {
+    async fn hello(&self, req: Request<HelloRequest>) -> Result<Response<HelloResponse>, Status> {
+        let message = req.into_inner().message;
+        println!("Got a request {:?}", &message);
+
+        Ok(Response::new(HelloResponse {
+            message: format!("Got a request: {:?}", &message),
+        }))
+    }
+}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let first_controller = ImplFirstController::default();
+
+    println!("LISTENING ON {:?}", addr);
+    Server::builder()
+        .add_service(FirstControllerServer::new(first_controller))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
